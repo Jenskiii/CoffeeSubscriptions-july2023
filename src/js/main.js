07365948420e1a5f83624992f1203280ace943b3
Jenskiii <124568361+Jenskiii@users.noreply.github.com>
2023-07-21 09:29:49 +0200

    added plan page + end screen

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,19 @@
 const hamburger = document.querySelector(".toggle__nav");
 const primaryNav = document.querySelector(".primary__nav--list");
+const navList = document.querySelectorAll(".primary__nav--list");
 hamburger.addEventListener("click", toggleNav);
 
 function toggleNav(){
     hamburger.classList.toggle("active");
     primaryNav.classList.toggle("active");
+}
+
+// resets nav when nav or body is clicked
+navList.forEach(e => {
+    e.addEventListener("click", resetNav)
+})
+
+function resetNav(){
+    hamburger.classList.remove("active");
+    primaryNav.classList.remove("active");
 }
--- src/js/plan.js
+++ src/js/plan.js
@@ -1,5 +1,10 @@
-// accordion
+////////////////
+// accordion //
+//////////////
 const accordionOpener = document.querySelectorAll(".accordion__title");
+const allArrows = document.querySelectorAll(".arrow-button");
+
+//opens and closes accordion
 accordionOpener.forEach( (e) => e.addEventListener("click", ()=>{
     let arrow = e.childNodes[3];
     // opens item and closes it
@@ -10,6 +15,10 @@ accordionOpener.forEach( (e) => e.addEventListener("click", ()=>{
         const openedItems = document.querySelectorAll(".is-open");
         openedItems.forEach((item) => {
         item.classList.remove("is-open")})
+        // resets all arrows to default to prevent double opened
+        allArrows.forEach(e => {
+            e.setAttribute("aria-selected", false)
+        })
         // opens clicked item
         e.classList.add("is-open");
     }
@@ -21,13 +30,21 @@ accordionOpener.forEach( (e) => e.addEventListener("click", ()=>{
     }  
 }));
 
-
-
-//cards
+//////////
+//cards//
+////////
 const cards = document.querySelectorAll(".card");
 cards.forEach(e => e.addEventListener("click", handleOption));
-// title
+// title  //
 const titles = document.querySelectorAll(".accordion__title");
+
+// select current column when click on a card
+function handleOption(e){
+    let selected = this.getAttribute("aria-label");
+    selectOption(e, document.querySelectorAll(`.${selected} > div`));
+    handleOrderSummary();
+    handleButton();
+}
 
 // sets card to active on click and highlights title
 function selectOption(e ,column){
@@ -44,16 +61,7 @@ function selectOption(e ,column){
     handleValue(e)
 }
 
-// select current column when click on a card
-function handleOption(e){
-    let selected = this.getAttribute("aria-label");
-    selectOption(e ,document.querySelectorAll(`.${selected} > div`));
-    handleOrderSummary();
-    handleButton();
-}
-
-
-// value of option
+// saves value of option to local storage
 function handleValue(e){
     let id = e.currentTarget.id;
     let name = e.currentTarget.parentNode.id;
@@ -61,22 +69,47 @@ function handleValue(e){
       localStorage.setItem(name ,id)
     }
 }
-
-// order summary
+//////////////////
+//order summary//
+////////////////
 window.addEventListener("load", handleOrderSummary);
-// sets value inside html if not selected value is ______
+// resets plan on page refresh
+window.onload = localStorage.clear()
+// sets value inside html if not selected value is ______ and highlight aside title
 function handleOrderSummary(){
     let preferences = localStorage.getItem("preferences") || "_____";
     let type = localStorage.getItem("type") || "_____";
-    let amount = localStorage.getItem("amount") || "_____";
+    let amount = localStorage.getItem("quantity") || "_____";
     let grind = localStorage.getItem("grind") || "_____";
     let frequency = localStorage.getItem("frequency") || "_____";
-
-    const order = document.querySelector(".order");
-    order.innerHTML = `“I drink my coffee as <span>${preferences}</span>, with a <span>${type}</span> type of bean.
+    // changes innerHTMl of plan + check out screen
+    const order = document.querySelectorAll(".order");
+    order.forEach(e => {
+        e.innerHTML = `“I drink my coffee as <span>${preferences}</span>, with a <span>${type}</span> type of bean.
      <span>${amount}</span> ground ala <span>${grind}</span>, sent to me <span>${frequency}</span>.”`
+    })
+    
+     // highlights aside titles in htlm (desktop)
+     if(preferences !== "_____"){
+        document.querySelector(".survey__aside--preferences").setAttribute("aria-selected", "true")
+     }
+     if(type !== "_____"){
+        document.querySelector(".survey__aside--type").setAttribute("aria-selected", "true")
+     }
+     if(amount !== "_____"){
+        document.querySelector(".survey__aside--quantity").setAttribute("aria-selected", "true")
+     }
+     if(grind !== "_____"){
+        document.querySelector(".survey__aside--grind").setAttribute("aria-selected", "true")
+     }
+     if(frequency !== "_____"){
+        document.querySelector(".survey__aside--frequency").setAttribute("aria-selected", "true")
+     }
 }
 
+//////////////////
+//order button //
+////////////////
 // set button to ready if every option is selected
 function handleButton(){
     const orderButton = document.querySelector(".order-button");
@@ -88,4 +121,33 @@ function handleButton(){
             orderButton.setAttribute("aria-selected", "false");
         }
     }  
+    // adds click function to button if active otherwise removes it
+    if(orderButton.getAttribute("aria-selected") === "true"){
+        orderButton.addEventListener("click", handleEndScreen);
+    }else{
+        orderButton.removeEventListener("click", handleEndScreen);
+    }
+}
+// shows end screen + makes bg faded 
+// also scrolls to top on click
+const overlay = document.querySelector(".overlay");
+const endScreen = document.querySelector(".end__screen");
+function handleEndScreen(){
+    // shows overlay + end screen
+    overlay.classList.add("active");
+    endScreen.classList.add("active");
+    // scrolls to top on button click
+    window.scrollTo(0, 0);
+}
+
+//////////////////////
+// checkout button //
+////////////////////
+const checkoutButton = document.querySelector(".end__button");
+checkoutButton.addEventListener("click",handleCheckout);
+
+function handleCheckout(){
+    overlay.classList.remove("active");
+    endScreen.classList.remove("active");
+    localStorage.clear();
 }
